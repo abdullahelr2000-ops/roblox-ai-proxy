d8d71b2cf96d97a6d3b121e54377f605a7d79fc5
abdullah <abdullah12@gmail.com>
2026-06-12 09:28:14 -0700

    fixed code

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.post("/generate", async (req, res) => {
 
   try {
     const response = await axios.post(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-3.5-flash:generateContent?key=${GEMINI_API_KEY}`,
+     `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${GEMINI_API_KEY}`,
       {
         contents: [
           { 
